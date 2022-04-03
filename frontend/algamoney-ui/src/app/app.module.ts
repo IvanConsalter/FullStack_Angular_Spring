f903d0441f07a79df1313b2766e099953842b7d5
Ivan Consalter <ivanconsalter1@gmail.com>
2022-04-03 16:36:35 -0300

    added search form

--- frontend/algamoney-ui/src/app/app.module.ts
+++ frontend/algamoney-ui/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Primeng
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
