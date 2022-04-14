5cdfaf325b779390306b400e9ea5514c9c5a72a9
Ivan Consalter <ivanconsalter1@gmail.com>
2022-04-13 21:28:31 -0300

    added lancamentos.module

--- frontend/algamoney-ui/src/app/app.module.ts
+++ frontend/algamoney-ui/src/app/app.module.ts
@@ -21,32 +21,30 @@ import { MessageModule } from 'primeng/message';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 // My Modules
+import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppComponent } from './app.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
 import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent,
     PessoaCadastroComponent,
     MessageComponent,
-    LancamentosGridComponent,
     PessoasGridComponent
   ],
   imports: [
+    // Angular Modules
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    // My Modules
+    LancamentosModule,
     // PrimeNg
     InputTextModule,
     ButtonModule,
---
+++ frontend/algamoney-ui/src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// PrimeNg
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+// import { InputMaskModule } from 'primeng/inputmask';
+
+// ng2-currency
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+// my modules
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+
+@NgModule({
+  declarations: [
+    LancamentosPesquisaComponent,
+    LancamentoCadastroComponent,
+    LancamentosGridComponent
+  ],
+  imports: [
+    // Angular Modules
+    CommonModule,
+    FormsModule,
+    // PrimeNg Modules
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    // InputMaskModule,
+    // ng2-currency
+    CurrencyMaskModule
+  ],
+  exports: [
+    LancamentosPesquisaComponent,
+    LancamentoCadastroComponent
+  ]
+})
+export class LancamentosModule { }
