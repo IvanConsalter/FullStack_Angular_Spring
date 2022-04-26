276ce96b1c1bf103660fd9213da1bad58a2fb2f9
Ivan Consalter <ivanconsalter1@gmail.com>
2022-04-26 19:36:42 -0300

    organizing app.module to core.module

--- frontend/algamoney-ui/src/app/app.module.ts
+++ frontend/algamoney-ui/src/app/app.module.ts
@@ -1,14 +1,7 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-
-// PrimeNg
-import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ConfirmationService } from 'primeng/api';
 
 // My Components
 import { AppComponent } from './app.component';
@@ -17,22 +10,6 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
-
-// my services
-import { LancamentoService } from './lancamentos/lancamento.service';
-import { PessoaService } from './pessoas/pessoa.service';
-
-// Translate
-
-// Registra a Localização
-import localePt from '@angular/common/locales/pt';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-registerLocaleData(localePt, 'pt-BR');
-
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [
@@ -46,26 +23,9 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     // My Modules
     CoreModule,
     LancamentosModule,
-    PessoasModule,
-    // PrimeNg
-    ToastModule,
-    ConfirmDialogModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
+    PessoasModule
   ],
-  providers: [
-    LancamentoService,
-    PessoaService,
-    // PrimeNg
-    MessageService,
-    ConfirmationService,
-    TranslateService
-  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/algamoney-ui/src/app/core/core.module.ts
+++ frontend/algamoney-ui/src/app/core/core.module.ts
@@ -1,25 +1,61 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
+// PrimeNg
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+// Services
+import { LancamentoService } from '../lancamentos/lancamento.service';
+import { PessoaService } from '../pessoas/pessoa.service';
+
+// Components
 import { NavbarComponent } from './navbar/navbar.component';
 
+// Translate
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import localePt from '@angular/common/locales/pt';
-
 registerLocaleData(localePt, 'pt-BR');
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // PrimeNg
+    ToastModule,
+    ConfirmDialogModule,
+    // Translate
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+
+    ToastModule,
+    ConfirmDialogModule
   ],
   providers: [
     DatePipe,
-    {provide: LOCALE_ID, useValue: 'pt-BR'}
+    LancamentoService,
+    PessoaService,
+    // PrimeNg
+    MessageService,
+    ConfirmationService,
+    TranslateService
   ]
 })
 export class CoreModule { }
