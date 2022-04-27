7a9ff27d8cb9dac74b500bc95f7f0265a3f8a107
Ivan Consalter <ivanconsalter1@gmail.com>
2022-04-27 08:48:46 -0300

    improving error message when deleting a person

--- frontend/algamoney-ui/src/app/core/error-handler.service.ts
+++ frontend/algamoney-ui/src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -16,9 +17,21 @@ export class ErrorHandlerService {
     if (typeof respostaErro === 'string') {
       mensagem = respostaErro;
     }
+    else if (
+        respostaErro instanceof HttpErrorResponse &&
+        respostaErro.status >= 400 &&
+        respostaErro.status <= 499) {
+      mensagem = 'Ocorreu um erro ao processar a sua solicitação!';
+
+      try {
+        mensagem = respostaErro.error[0].mensagemUsuario;
+      } catch (error) {
+        console.error('Ocorreu um erro', respostaErro);
+      }
+    }
     else {
       mensagem = 'Erro ao processar o serviço remoto. Tente mais tarde novamente!';
-      console.log('Ocorreu um erro!', respostaErro);
+      console.error('Ocorreu um erro!', respostaErro);
     }
 
     this.messageService.add({severity: 'error', detail: mensagem, life: 10000});
