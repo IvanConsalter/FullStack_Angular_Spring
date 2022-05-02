c69c217fcc8b83dbc04e1412d9fe0be39009f167
Ivan Consalter <ivanconsalter1@gmail.com>
2022-05-02 18:01:55 -0300

    configuring routes

--- frontend/algamoney-ui/src/app/app.module.ts
+++ frontend/algamoney-ui/src/app/app.module.ts
@@ -2,14 +2,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 // My Components
 import { AppComponent } from './app.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 // my modules
 import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+
+export const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,6 +30,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
     // My Modules
     CoreModule,
     LancamentosModule,
--- frontend/algamoney-ui/src/app/core/navbar/navbar.component.ts
+++ frontend/algamoney-ui/src/app/core/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  public mostrarMenu: boolean;
+  public mostrarMenu = true;
 
   constructor() { }
 
