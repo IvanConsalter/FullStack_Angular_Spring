b8aeca44608f398b8773e01cc9e50fa1f5f427a1
Ivan Consalter <ivanconsalter1@gmail.com>
2022-05-18 16:00:31 -0300

    added login-form

--- frontend/algamoney-ui/src/app/app.component.ts
+++ frontend/algamoney-ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,21 @@ export class AppComponent implements OnInit{
 
   constructor(
     private config: PrimeNGConfig,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.translateService.setDefaultLang('pt');
     this.translateService.get('primeng')
       .subscribe( resposta => this.config.setTranslation(resposta));
+  }
+
+  exibirNavBar(): boolean {
+    if (this.router.url !== '/login') {
+      return true;
+    }
+
+    return false;
   }
 }
---
+++ frontend/algamoney-ui/src/app/seguranca/seguranca.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { LoginFormComponent } from './login-form/login-form.component';
+
+import { SegurancaRoutingModule } from './seguranca-routing.module';
+
+@NgModule({
+  declarations: [
+    LoginFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+
+    SegurancaRoutingModule
+  ]
+})
+
+export class SegurancaModule { }
