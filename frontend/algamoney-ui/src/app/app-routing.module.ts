7d9afde8dfc8a0c7b31229ef06dd21443cd0bed4
Ivan Consalter <ivanconsalter1@gmail.com>
2022-05-27 18:22:32 -0300

    redirecting to login page if there is no token

--- frontend/algamoney-ui/src/app/app-routing.module.ts
+++ frontend/algamoney-ui/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 import { NaoAutorizadoComponent } from './core/nao-autorizado/nao-autorizado.component';
+import { LoginFormComponent } from './seguranca/login-form/login-form.component';
 
 const routes: Routes = [
+  { path: '', component: LancamentosPesquisaComponent},
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
   { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' }
--- frontend/algamoney-ui/src/app/seguranca/auth.guard.ts
+++ frontend/algamoney-ui/src/app/seguranca/auth.guard.ts
@@ -18,7 +18,20 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (route.data.roles &&  !this.authService.temQualquerPermissao(route.data.roles)) {
+    if (this.authService.isAccessTokenInvalido()) {
+      console.log('Navegando com access token inválido. Obtendo novo token...');
+
+      return this.authService.obterNovoAccessToken()
+        .then( () => {
+          if (this.authService.isAccessTokenInvalido()) {
+            this.router.navigate(['/login']);
+            return false;
+          }
+
+          return true;
+        });
+    }
+    else if (route.data.roles &&  !this.authService.temQualquerPermissao(route.data.roles)) {
       this.router.navigate(['/nao-autorizado']);
       return false;
     }
