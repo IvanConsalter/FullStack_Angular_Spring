335bd055c31271c522a646a0db2dde8a415aa7a2
Ivan Consalter <ivanconsalter1@gmail.com>
2022-12-28 13:46:35 -0300

    added RecursoCriadoEvent && RecursoCriadoListener

--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/resource/CategoriaResource.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/resource/CategoriaResource.java
@@ -1,12 +1,13 @@
 package com.ivanconsalter.algamoney.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ivanconsalter.algamoney.event.RecursoCriadoEvent;
 import com.ivanconsalter.algamoney.model.Categoria;
 import com.ivanconsalter.algamoney.repository.CategoriaRepository;
 
@@ -25,6 +26,9 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
 	
 	@GetMapping
 	public List<Categoria> listar() {
@@ -38,11 +42,9 @@ public class CategoriaResource {
 	) {
 		Categoria novaCategoria = categoriaRepository.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoria.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		eventPublisher.publishEvent(new RecursoCriadoEvent(this, response, novaCategoria.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(novaCategoria);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novaCategoria);
 	}
 	
 	@GetMapping(path = "/{codigo}")
