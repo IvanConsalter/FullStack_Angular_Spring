61dc489521a377630d758ac080818afd915cfda7
Ivan Consalter <ivanconsalter1@gmail.com>
2022-12-30 16:02:09 -0300

    added buscarPorCodigo in LancamentoResource

--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/resource/LancamentoResource.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/resource/LancamentoResource.java
@@ -3,7 +3,9 @@ package com.ivanconsalter.algamoney.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,12 @@ public class LancamentoResource {
 	@GetMapping()
 	public List<Lancamento> listar() {
 		return lancamentoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{codigo}")
+	public ResponseEntity<Lancamento> buscarPorCodigo(@PathVariable Long codigo) {
+		return lancamentoRepository.findById(codigo)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
