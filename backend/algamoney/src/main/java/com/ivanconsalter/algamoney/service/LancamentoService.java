5383a223b8a8432dbf9f869c3c997e5b4a55b169
Ivan Consalter <ivanconsalter1@gmail.com>
2023-01-30 18:19:15 -0300

    added update in Lancamento

--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/service/LancamentoService.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/service/LancamentoService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,13 +26,41 @@ public class LancamentoService {
 	public Lancamento salvar(@Valid Lancamento lancamento) {
 		
 		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
-		if(pessoa.isEmpty() || pessoa.get().isInativo()) {
-			throw new PessoaInexistenteOuInativaException();
-		}
+		verificarSePessoaExisteOuAtiva(pessoa);
 		
 		return lancamentoRepository.save(lancamento);
 	}
 	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		
+		if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+		
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+	
+	private void validarPessoa(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = null;
+		
+		if(lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+		
+		verificarSePessoaExisteOuAtiva(pessoa);
+	}
+	
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		return lancamentoRepository.findById(codigo).orElseThrow( () -> new IllegalArgumentException());
+	}
+	
+	private void verificarSePessoaExisteOuAtiva(Optional<Pessoa> pessoa) {
+		if(pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
 	
 	
 }
