17ee77d785622e3836e51a8db0674cd9ec6a711a
Ivan Consalter <ivanconsalter1@gmail.com>
2023-01-30 17:31:02 -0300

    added findByNomeContaing in Pessoa

--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/repository/PessoaRepository.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.ivanconsalter.algamoney.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ivanconsalter.algamoney.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
