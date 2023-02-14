d61c9edbc9ca95256cdcbb3284c352fa2dce022e
Ivan Consalter <ivanconsalter1@gmail.com>
2023-02-14 17:00:26 -0300

    added Contato and relationship

---
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/model/Contato.java
@@ -0,0 +1,100 @@
+package com.ivanconsalter.algamoney.model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank
+	private String nome;
+	
+	@Email
+	@NotBlank
+	private String email;
+	
+	@NotBlank
+	private String telefone;
+	
+	@ManyToOne()
+	@JoinColumn(name = "codigo_pessoa")
+	@NotNull
+	private Pessoa pessoa;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+
+	@Override
+	public String toString() {
+		return "Contato [codigo=" + codigo + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone
+				+ ", pessoa=" + pessoa + "]";
+	}
+	
+}
