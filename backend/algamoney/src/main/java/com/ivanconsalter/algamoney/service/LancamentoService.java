720e9f0260610b0c4abd33254ec615cadc52e100
Ivan Consalter <ivanconsalter1@gmail.com>
2023-02-21 14:05:44 -0300

    updating && deleting a Anexo

--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/service/LancamentoService.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/service/LancamentoService.java
@@ -38,113 +38,119 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 @Service
 public class LancamentoService {
 
-	private static final String DESTINATARIOS =  AuthorityEnum.ROLE_PESQUISAR_LANCAMENTO.toString();
-	
+	private static final String DESTINATARIOS = AuthorityEnum.ROLE_PESQUISAR_LANCAMENTO.toString();
+
 	private static final Logger logger = LoggerFactory.getLogger(Lancamento.class);
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private Mailer mailer;
-	
+
 	@Autowired
 	private S3 s3;
-	
+
 //	@Scheduled(cron = "0 0 6 * * *")
 //	@Scheduled(fixedDelay = 1000 * 60 * 30)
 	public void avisarSobreLancamentoVencido() {
-		if(logger.isDebugEnabled()) {
-			logger.debug("Preparando envio de "
-					+ "e-mails de aviso de lançamentos vencidos.");
+		if (logger.isDebugEnabled()) {
+			logger.debug("Preparando envio de " + "e-mails de aviso de lançamentos vencidos.");
 		}
-		
-		List<Lancamento> listLancamentosVencidos = lancamentoRepository.findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
-		
-		if(listLancamentosVencidos.isEmpty()) {
+
+		List<Lancamento> listLancamentosVencidos = lancamentoRepository
+				.findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
+
+		if (listLancamentosVencidos.isEmpty()) {
 			logger.info("Sem lançamentos vencidos para aviso.");
 			return;
 		}
-		
+
 		logger.info("Existem {} lançamentos vencidos.", listLancamentosVencidos.size());
-		
+
 		List<Usuario> destinatarios = usuarioRepository.findByPermissoesDescricao(DESTINATARIOS);
-		
-		if(destinatarios.isEmpty()) {
-			logger.warn("Existem lançamentos vencidos, mas o "
-					+ "sistema não encontrou destinatários.");
+
+		if (destinatarios.isEmpty()) {
+			logger.warn("Existem lançamentos vencidos, mas o " + "sistema não encontrou destinatários.");
 			return;
 		}
-		
+
 		mailer.avisarSobreLancamentosVencidos(listLancamentosVencidos, destinatarios);
-		
+
 		logger.info("Envio de e-mail de aviso concluído.");
 	}
 
 	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
 		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
-		
+
 		Map<String, Object> parametros = new HashMap<>();
 		parametros.put("DT_INICIO", Date.valueOf(inicio));
 		parametros.put("DT_FIM", Date.valueOf(fim));
 		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
-		
+
 		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/lancamentos-por-pessoa.jasper");
-		
-		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros, new JRBeanCollectionDataSource(dados));
-		
+
+		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros,
+				new JRBeanCollectionDataSource(dados));
+
 		return JasperExportManager.exportReportToPdf(jasperPrint);
-		
+
 	}
-	
+
 	public Lancamento salvar(@Valid Lancamento lancamento) {
-		
+
 		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
 		verificarSePessoaExisteOuAtiva(pessoa);
-		
-		if(StringUtils.hasText(lancamento.getAnexo())) {
+
+		if (StringUtils.hasText(lancamento.getAnexo())) {
 			s3.salvar(lancamento.getAnexo());
 		}
-		
+
 		return lancamentoRepository.save(lancamento);
 	}
-	
+
 	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
 		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
-		
-		if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
 			validarPessoa(lancamento);
 		}
-		
+
+		if (!StringUtils.hasLength(lancamento.getAnexo()) && StringUtils.hasText(lancamentoSalvo.getAnexo())) {
+			s3.remover(lancamentoSalvo.getAnexo());
+		} else if (StringUtils.hasLength(lancamento.getAnexo())
+				&& !lancamento.getAnexo().equals(lancamentoSalvo.getAnexo())) {
+			s3.substituirAnexo(lancamentoSalvo.getAnexo(), lancamento.getAnexo());
+		}
+
 		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
 		return lancamentoRepository.save(lancamentoSalvo);
 	}
-	
+
 	private void validarPessoa(Lancamento lancamento) {
 		Optional<Pessoa> pessoa = null;
-		
-		if(lancamento.getPessoa().getCodigo() != null) {
+
+		if (lancamento.getPessoa().getCodigo() != null) {
 			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
 		}
-		
+
 		verificarSePessoaExisteOuAtiva(pessoa);
 	}
-	
+
 	private Lancamento buscarLancamentoExistente(Long codigo) {
-		return lancamentoRepository.findById(codigo).orElseThrow( () -> new IllegalArgumentException());
+		return lancamentoRepository.findById(codigo).orElseThrow(() -> new IllegalArgumentException());
 	}
-	
+
 	private void verificarSePessoaExisteOuAtiva(Optional<Pessoa> pessoa) {
-		if(pessoa.isEmpty() || pessoa.get().isInativo()) {
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
 	}
-	
-	
+
 }
--- backend/algamoney/src/main/java/com/ivanconsalter/algamoney/storage/S3.java
+++ backend/algamoney/src/main/java/com/ivanconsalter/algamoney/storage/S3.java
@@ -9,10 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.GroupGrantee;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.ObjectTagging;
@@ -24,47 +26,43 @@ import com.ivanconsalter.algamoney.config.property.AlgamoneyApiProperty;
 
 @Component
 public class S3 {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(S3.class);
-	
+
 	@Autowired
 	private AlgamoneyApiProperty algamoneyApiProperty;
 
 	@Autowired
 	private AmazonS3 amazonS3;
-	
+
 	public String salvarTemporariamente(MultipartFile arquivo) {
 		AccessControlList accessControlList = new AccessControlList();
 		accessControlList.grantPermission(GroupGrantee.AllUsers, Permission.Read);
-		
+
 		ObjectMetadata objectMetadata = new ObjectMetadata();
 		objectMetadata.setContentType(arquivo.getContentType());
 		objectMetadata.setContentLength(arquivo.getSize());
-		
+
 		String nomeUnico = gerarNomeUnico(arquivo.getOriginalFilename());
-		
+
 		try {
-			PutObjectRequest putObjectRequest = new PutObjectRequest(
-					algamoneyApiProperty.getS3().getBucket(),
-					nomeUnico,
-					arquivo.getInputStream(),
-					objectMetadata).withAccessControlList(accessControlList);
-			
+			PutObjectRequest putObjectRequest = new PutObjectRequest(algamoneyApiProperty.getS3().getBucket(),
+					nomeUnico, arquivo.getInputStream(), objectMetadata).withAccessControlList(accessControlList);
+
 			putObjectRequest.setTagging(new ObjectTagging(Arrays.asList(new Tag("expirar", "true"))));
-			
+
 			amazonS3.putObject(putObjectRequest);
-			
-			if(logger.isDebugEnabled()) {
+
+			if (logger.isDebugEnabled()) {
 				logger.debug("Arquivo {} enviado com sucesso para o S3.", arquivo.getOriginalFilename());
 			}
-			
+
 			return nomeUnico;
-			
+
 		} catch (IOException e) {
 			throw new RuntimeException("Problemas ao tentar enviar arquivo para o S3", e);
 		}
-				
-		
+
 	}
 
 	public String configurarUrl(String objeto) {
@@ -73,13 +71,26 @@ public class S3 {
 
 	public void salvar(String objeto) {
 		SetObjectTaggingRequest setObjectTaggingRequest = new SetObjectTaggingRequest(
-				algamoneyApiProperty.getS3().getBucket(),
-				objeto,
-				new ObjectTagging(Collections.emptyList()));
-		
+				algamoneyApiProperty.getS3().getBucket(), objeto, new ObjectTagging(Collections.emptyList()));
+
 		amazonS3.setObjectTagging(setObjectTaggingRequest);
 	}
-	
+
+	public void remover(String objeto) {
+		DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(algamoneyApiProperty.getS3().getBucket(),
+				objeto);
+
+		amazonS3.deleteObject(deleteObjectRequest);
+	}
+
+	public void substituirAnexo(String objetoAntigo, String objetoNovo) {
+		if(StringUtils.hasText(objetoAntigo)) {
+			this.remover(objetoAntigo);
+		}
+		
+		this.salvar(objetoNovo);
+	}
+
 	private String gerarNomeUnico(String originalFilename) {
 		return UUID.randomUUID().toString() + "_" + originalFilename;
 	}
